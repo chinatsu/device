13610588256355ad180a0744666b0241cc38e145
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2020-06-16 14:10:39 +0200

    add latest revision of scripts for reference

--- app/macos/helper/scripts/interface.sh
+++ app/macos/helper/scripts/interface.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 interface="$1"
 bootstrap_config="$2"
 pid_path=$3
@@ -7,15 +7,27 @@ mtu=1360
 wireguard_go=/opt/naisdevice/bin/naisdevice-wireguard-go
 jq=/opt/naisdevice/bin/jq
 pkill=/usr/bin/pkill
+pgrep=/usr/bin/pgrep
 ifconfig=/sbin/ifconfig
 route=/sbin/route
 ps=/bin/ps
 
+teardown() {
+  if $pgrep -f "$wireguard_go $interface"; then
+    echo "stopping wireguard-go"
+    $pkill -f "$wireguard_go $interface"
+  else
+    echo "wireguard-go already stopped"
+  fi
+}
+
+echo "setting up interface"
 if [[ -d "$pid_path" ]]; then
   echo "$pid_path not found, no agent running"
+  teardown
+  exit 1
 fi
 
-agent_pid=$(cat $pid_path)
 
 if [[ ! -f "$bootstrap_config" ]]; then
   echo "no bootstrap config found"
@@ -23,24 +35,23 @@ if [[ ! -f "$bootstrap_config" ]]; then
 fi
 
 ip=$($jq -r .deviceIP < "$bootstrap_config")
-echo $ip
 
-if $ps -P $agent_pid; then
+agent_pid=$(cat "$pid_path")
+if $ps -p "$agent_pid"; then
   if ! $pgrep -f "$wireguard_go" ; then
     echo "running wireguard-go"
-    $wireguard_go "$interface"
+    $wireguard_go -f "$interface" &
+    disown
+    sleep 1
     $ifconfig "$interface" inet "${ip}/21" "$ip" add
     $ifconfig "$interface" mtu "$mtu"
     $ifconfig "$interface" up
-    $route -q -n add -inet "${ip}/21" -interface "$interface"
+    #$route -q -n add -inet "${ip}/21" -interface "$interface"
   else
     echo "wireguard-go already running"
   fi
 else
-  if $pgrep -f "$wireguard_go $interface"; then
-    echo "stopping wireguard-go"
-    $pkill -f "$wireguard_go $interface"
-  else
-    echo "wireguard-go already stopped"
-  fi
+  teardown
 fi
+
+sleep 20
--- app/macos/helper/scripts/sync.sh
+++ app/macos/helper/scripts/sync.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 interface="$1"
 conf="$2"
 
@@ -13,16 +13,16 @@ else
   exit 1
 fi
 
-declare -A desiered_routes
-for i in $(while read -r _ ips; do for ip in $ips; do [[ $ip =~ ^[0-9a-z:.]+/[0-9]+$ ]] && echo "$ip"; done; done < <($wg show "$interface" allowed-ips) | sort -nr -k 2 -t /); do
-  desiered_routes[$i]=true
-done
-
-for r in "${!desiered_routes[@]}"; do
-  r=${r%"/32"}
-  ri=$($route get "$r" | grep interface | cut -d ':' -f 2 | tr -d ' ')
-  if [[ "$ri" != "$interface" ]]; then
-    echo $route -q -n add -inet $r -interface "$interface"
-    $route -q -n add -inet $r -interface "$interface"
-  fi
-done
+#declare -A desiered_routes
+#for i in $(while read -r _ ips; do for ip in $ips; do [[ $ip =~ ^[0-9a-z:.]+/[0-9]+$ ]] && echo "$ip"; done; done < <($wg show "$interface" allowed-ips) | sort -nr -k 2 -t /); do
+#  desiered_routes[$i]=true
+#done
+#
+#for r in "${!desiered_routes[@]}"; do
+#  r=${r%"/32"}
+#  ri=$($route get "$r" | grep interface | cut -d ':' -f 2 | tr -d ' ')
+#  if [[ "$ri" != "$interface" ]]; then
+#    echo $route -q -n add -inet $r -interface "$interface"
+#    $route -q -n add -inet $r -interface "$interface"
+#  fi
+#done
